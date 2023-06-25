e604799e8e4ec134e509947fde64db98fc6a41df
tonymatheus <tony.chaves14@gmail.com>
2023-06-25 01:41:23 -0300

    Add textComponent with  font size and line height standard

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {Text as RNText, TextProps, TextStyle} from 'react-native';
+
+export const Text = ({children, ...rest}: TextProps) => {
+  return (
+    <RNText style={fontSizes.headingLarge} {...rest}>
+      {children}
+    </RNText>
+  );
+};
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+//fontsize, lineHeight
+const fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
