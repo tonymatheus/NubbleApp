50dd6bc7e4fcf83b08a530f15a76246e187e67ab
tonymatheus <tony.chaves14@gmail.com>
2023-08-12 00:07:56 -0300

    Add theme provider in app

--- App.tsx
+++ App.tsx
@@ -10,24 +10,21 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './theme/theme';
 
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text bold preset="headingLarge">
-        Coffstack
-      </Text>
-      <View
-        style={{
-          marginTop: 20,
-          marginLeft: 20,
-          marginRight: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Button title={'Teste'} />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text bold preset="headingLarge">
+          Coffstack
+        </Text>
+        <View>
+          <Button title={'Teste'} />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
