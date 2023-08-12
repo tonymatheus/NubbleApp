fe063fe9a1d6f75f2218a36bafc57bbc95285667
tonymatheus <tony.chaves14@gmail.com>
2023-08-12 00:08:36 -0300

    Add theme styles in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 
 import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
+import {useTheme} from '@shopify/restyle';
+import {Theme} from '../../../theme/theme';
 
 interface ButtonProps {
   title: string;
 }
 
 export const Button = ({title}: ButtonProps) => {
+  const {colors} = useTheme<Theme>();
+
   return (
     <TouchableOpacity
       style={{
         borderRadius: 18,
         paddingVertical: 14,
         paddingHorizontal: 20,
-        backgroundColor: '#074c4e',
+        backgroundColor: colors.carrotSecondary,
 
         alignItems: 'center',
         width: '100%',
