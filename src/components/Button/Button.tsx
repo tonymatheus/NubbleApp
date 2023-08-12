1b20ef3555c9fbc89b2a58efdef8c989848b3882
tonymatheus <tony.chaves14@gmail.com>
2023-08-12 00:40:20 -0300

    Add Box component in buttom

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
 
-import {TouchableOpacity} from 'react-native';
+import {ActivityIndicator, TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../../../theme/theme';
+import {Box} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
+  loading?: boolean;
 }
 
-export const Button = ({title}: ButtonProps) => {
+export const Button = ({title, loading}: ButtonProps) => {
   const {colors} = useTheme<Theme>();
 
   return (
-    <TouchableOpacity
-      style={{
-        borderRadius: 18,
-        paddingVertical: 14,
-        paddingHorizontal: 20,
-        backgroundColor: colors.carrotSecondary,
+    <Box
+      backgroundColor="buttonPrimary"
+      paddingHorizontal="s10"
+      paddingVertical="s10"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
+      //   style={{
+      //     borderRadius: 18,
+      //     paddingVertical: 14,
+      //     paddingHorizontal: 20,
+      //     backgroundColor: colors.carrotSecondary,
 
-        alignItems: 'center',
-        width: '100%',
-      }}>
-      <Text preset="headingMedium" style={{color: '#fff'}}>
-        {title}
-      </Text>
-    </TouchableOpacity>
+      //     alignItems: 'center',
+      //     width: '100%',
+    >
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
+          {title}
+        </Text>
+      )}
+    </Box>
   );
 };
