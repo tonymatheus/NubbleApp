1c1b765eb05876add8b57b22570b36fb80054d6e
tonymatheus <tony.chaves14@gmail.com>
2023-08-15 05:55:29 -0300

    Add Restyle to TextComponents and add properteies

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import {
-  Text as RNText,
-  TextProps as RNTextProps,
-  TextStyle,
-} from 'react-native';
-interface TextProps extends RNTextProps {
+import {TextStyle} from 'react-native';
+import {createText} from '@shopify/restyle';
+import {Theme} from '../../../theme/theme';
+
+const SRText = createText<Theme>();
+type SRTextProps = React.ComponentProps<typeof SRText>;
+
+interface TextProps extends SRTextProps {
   preset?: TextVariants;
   bold?: boolean;
   italic?: boolean;
@@ -18,14 +20,17 @@ export const Text = ({
   italic,
   semiBold,
   preset = 'paragraphMedium',
-  ...rest
+  ...SRTextProps
 }: TextProps) => {
   const fontFamily = getFontFamily(preset, bold, italic, semiBold);
 
   return (
-    <RNText style={[$fontSizes[preset], {fontFamily}, style]} {...rest}>
+    <SRText
+      color="backgroundContrast"
+      style={[$fontSizes[preset], {fontFamily}, style]}
+      {...SRTextProps}>
       {children}
-    </RNText>
+    </SRText>
   );
 };
 
