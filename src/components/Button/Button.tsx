04ae4a40206e5bc5c5db99a97dae0064fab4fa42
tonymatheus <tony.chaves14@gmail.com>
2023-08-15 11:31:10 -0300

    Start to Add button presets with types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,31 +3,41 @@ import React from 'react';
 import {ActivityIndicator} from 'react-native';
 import {Text} from '../Text/Text';
 import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {ThemeColors} from '../../../theme/theme';
+import {buttonPresets} from './ButtonPresets';
+
+export type ButtonPreset = 'primary' | 'outline';
 
 interface ButtonProps extends TouchableOpacityBoxProps {
   title: string;
   loading?: boolean;
+  preset?: ButtonPreset;
+  disabled: boolean;
 }
 
 export const Button = ({
   title,
   loading,
+  preset = 'primary',
+  disabled,
   ...TouchableOpacityBoxProps
 }: ButtonProps) => {
+  const buttonPreset = buttonPresets[preset];
+
   return (
     <TouchableOpacityBox
-      backgroundColor="greenPrimary"
       paddingHorizontal="s10"
       paddingVertical="s10"
       height={50}
       alignItems="center"
       justifyContent="center"
       borderRadius="s16"
+      {...buttonPreset}
       {...TouchableOpacityBoxProps}>
       {loading ? (
         <ActivityIndicator />
       ) : (
-        <Text preset="paragraphMedium" bold color="primaryContrast">
+        <Text preset="paragraphMedium" bold color={buttonPreset.content}>
           {title}
         </Text>
       )}
