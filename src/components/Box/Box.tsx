c24114ccd08d8245fb21e628ba484251c3bc58ae
tonymatheus <tony.chaves14@gmail.com>
2023-08-15 05:56:12 -0300

    Export Box Pops

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -20,6 +20,7 @@ import {
 } from 'react-native';
 
 export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
 
 export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
   SpacingProps<Theme> &
