bd4a80cecd327ac7c306b538db7da76722a2aa00
tonymatheus <tony.chaves14@gmail.com>
2023-08-15 05:22:13 -0300

    Add custom Touchableopacity component With stle properties

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,5 +1,37 @@
-import {createBox} from '@shopify/restyle';
+import {
+  createBox,
+  createRestyleComponent,
+  backgroundColor,
+  BackgroundColorProps,
+  spacing,
+  SpacingProps,
+  layout,
+  LayoutProps,
+  border,
+  BorderProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
 
 import {Theme} from '../../../theme/theme';
+import {
+  TouchableOpacity,
+  TouchableOpacityProps as RNTouchableOpacityProps,
+} from 'react-native';
 
 export const Box = createBox<Theme>();
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  RNTouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);
