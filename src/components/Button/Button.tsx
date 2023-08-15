9632ca4b465728a1f605f55282ec7bf8e0c6a7e7
tonymatheus <tony.chaves14@gmail.com>
2023-08-15 05:22:53 -0300

    Apply styles in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,7 @@ import {ActivityIndicator, TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../../../theme/theme';
-import {Box} from '../Box/Box';
+import {Box, TouchableOpacityBox} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
@@ -15,8 +15,8 @@ export const Button = ({title, loading}: ButtonProps) => {
   const {colors} = useTheme<Theme>();
 
   return (
-    <Box
-      backgroundColor="buttonPrimary"
+    <TouchableOpacityBox
+      backgroundColor="carrotSecondary"
       paddingHorizontal="s10"
       paddingVertical="s10"
       height={50}
@@ -30,6 +30,6 @@ export const Button = ({title, loading}: ButtonProps) => {
           {title}
         </Text>
       )}
-    </Box>
+    </TouchableOpacityBox>
   );
 };
