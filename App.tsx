bde9b70afcbc18c7f93e0a8202fc3de8774bff2d
tony.matheus <tony.chaves14@gmail.com>
2023-08-18 05:49:19 -0300

    Apply Icon

--- App.tsx
+++ App.tsx
@@ -13,6 +13,9 @@ import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './theme/theme';
 import {Box} from './src/components/Box/Box';
+import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
+import {EyeOnIcon} from './src/assets/icons/EyeOnIcon';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
@@ -22,29 +25,8 @@ function App(): JSX.Element {
           <Text bold preset="headingLarge">
             Coffstack
           </Text>
-
-          <Button preset="primary" title={'entrar'} marginBottom="s12" />
-          <Button
-            disabled
-            preset="outline"
-            title={'outline'}
-            marginBottom="s12"
-          />
-
-          <Button
-            preset="outline"
-            loading
-            disabled
-            title={'Teste'}
-            marginBottom="s12"
-          />
-          <Button
-            preset="outline"
-            loading
-            disabled
-            title={'Teste'}
-            marginBottom="s12"
-          />
+          <Icon name="eyeOff" color="blue" size={50} />
+          <Icon name="eyeOn" color="blue" size={50} />
         </View>
       </SafeAreaView>
     </ThemeProvider>
