346d06c6cc09c1c753175dbe57a5999f839cb86b
tonymatheus <tony.chaves14@gmail.com>
2023-08-20 15:41:37 -0300

    Add new properties to icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {EyeOnIcon} from '../../assets/icons/EyeOnIcon';
 import {EyeOffIcon} from '../../assets/icons/EyeOffIcon';
+import {ThemeColors} from '../../../theme/theme';
+import {useAppTheme} from '../../hooks/useAppTheme';
+
+export interface IconBase {
+  size?: number;
+  color?: string;
+}
 
 interface Props {
   name: IconName;
+  color?: ThemeColors;
+  size?: number;
 }
 
-export const Icon = ({name}: Props) => {
+export const Icon = ({name, color = 'backgroundContrast', size}: Props) => {
+  const {colors} = useAppTheme();
   const SVGIcon = iconRegistry[name];
-  return <SVGIcon />;
+  return <SVGIcon color={colors[color]} size={size} />;
 };
 
 const iconRegistry = {
