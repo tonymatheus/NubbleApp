a715fa3ccfdc2e7ab6dcbb9438f7f8c610ab4a81
tonymatheus <tony.chaves14@gmail.com>
2023-08-21 05:50:08 -0300

    Remove Login values from app to separate components

--- App.tsx
+++ App.tsx
@@ -7,45 +7,18 @@
 
 import React from 'react';
 
-import {SafeAreaView, View} from 'react-native';
-import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './theme/theme';
-import {Button} from './src/components/Button/Button';
-import {TextInput} from './src/components/TextInput/TextInput';
-import {Icon} from './src/components/Icon/Icon';
+import {LoginScreen} from './src/screen/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text bold preset="headingLarge">
-            Olá
-          </Text>
-          <Text marginBottom="s40" preset="paragraphLarge">
-            Digite seu email e senha
-          </Text>
-          <TextInput
-            errorMessage="erro"
-            label="e-mail"
-            placeholder="email"
-            boxProps={{marginBottom: 's10'}}
-          />
-          <TextInput
-            label="senha"
-            placeholder="password"
-            RightComponent={<Icon color="gray2" name="eyeOn" />}
-            boxProps={{marginBottom: 's20'}}
-          />
-          <Text color="primary" preset="paragraphSmall" bold>
-            esqueci minha senha
-          </Text>
-          <Button title="Entrar" preset="primary" marginTop="s48" />
-          <Button title="Criar Conta" preset="outline" marginTop="s12" />
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
