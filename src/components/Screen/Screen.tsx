071067422c684a34898cebc301a543c81e55213e
tonymatheus <tony.chaves14@gmail.com>
2023-08-21 07:32:10 -0300

    Add Container logic to render View and scroll view

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import {Box} from '../Box/Box';
-import {Platform} from 'react-native';
+import {KeyboardAvoidingView, Platform} from 'react-native';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 import {Icon} from '../Icon/Icon';
 import {Text} from '../Text/Text';
+import {
+  ScrollViewContainer,
+  ViewContainer,
+} from './components/ScreenContainers';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 interface ScreenProps {
   children?: React.ReactNode;
   canGoback?: boolean;
+  scrollable?: boolean;
 }
 
-export const Screen = ({children, canGoback}: ScreenProps) => {
-  const {top} = useAppSafeArea();
+export const Screen = ({children, canGoback, scrollable}: ScreenProps) => {
+  const {top, bottom} = useAppSafeArea();
+  const {colors} = useAppTheme();
   console.log({
     device: Platform.OS,
     top,
   });
+
+  const Container = scrollable ? ScrollViewContainer : ViewContainer;
+
   return (
-    <Box paddingHorizontal="s24" style={{paddingTop: top}}>
-      {canGoback && (
-        <Box marginBottom="s24" flexDirection="row">
-          <Icon name="arrowLeft" color="primary" />
-          <Text preset="paragraphMedium" semiBold marginLeft="s8">
-            Voltar
-          </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <Container backgroundColor={colors.background}>
+        <Box
+          paddingBottom="s24"
+          paddingHorizontal="s24"
+          style={{paddingTop: top, paddingBottom: bottom}}>
+          {canGoback && (
+            <Box marginBottom="s24" flexDirection="row">
+              <Icon name="arrowLeft" color="primary" />
+              <Text preset="paragraphMedium" semiBold marginLeft="s8">
+                Voltar
+              </Text>
+            </Box>
+          )}
+          {children}
         </Box>
-      )}
-      {children}
-    </Box>
+      </Container>
+    </KeyboardAvoidingView>
   );
 };
