233409010f77ca8807782eb4e4376f09a22e86c6
tonymatheus <tony.chaves14@gmail.com>
2023-08-21 06:46:03 -0300

    Add Goback button with logic to visualize it

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import {Box} from '../Box/Box';
 import {Platform} from 'react-native';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
+import {Icon} from '../Icon/Icon';
+import {Text} from '../Text/Text';
 
 interface ScreenProps {
   children?: React.ReactNode;
+  canGoback?: boolean;
 }
 
-export const Screen = ({children}: ScreenProps) => {
+export const Screen = ({children, canGoback}: ScreenProps) => {
   const {top} = useAppSafeArea();
   console.log({
     device: Platform.OS,
@@ -15,6 +18,14 @@ export const Screen = ({children}: ScreenProps) => {
   });
   return (
     <Box paddingHorizontal="s24" style={{paddingTop: top}}>
+      {canGoback && (
+        <Box marginBottom="s24" flexDirection="row">
+          <Icon name="arrowLeft" color="primary" />
+          <Text preset="paragraphMedium" semiBold marginLeft="s8">
+            Voltar
+          </Text>
+        </Box>
+      )}
       {children}
     </Box>
   );
