66938e30f6ffb48d58ebb5779ec5dbada3e07c22
tonymatheus <tony.chaves14@gmail.com>
2023-08-21 07:29:13 -0300

    Add bottom property in useAppsafeArea Hook

--- src/hooks/useAppSafeArea.tsx
+++ src/hooks/useAppSafeArea.tsx
@@ -2,10 +2,11 @@ import {useAppTheme} from './useAppTheme';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const useAppSafeArea = () => {
-  const {top} = useSafeAreaInsets();
+  const {top, bottom} = useSafeAreaInsets();
   const {spacing} = useAppTheme();
 
   return {
     top: Math.max(top, spacing.s20),
+    bottom: Math.max(bottom, spacing.s20),
   };
 };
