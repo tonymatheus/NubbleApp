88db797ac496ab2b51d0784ded30e9d71c978802
tonymatheus <tony.chaves14@gmail.com>
2023-08-21 06:47:26 -0300

    Implements Auth signUpScreen

---
+++ src/screen/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {EyeOnIcon} from '../../../assets/icons/EyeOnIcon';
+import {Button} from '../../../components/Button/Button';
+
+export const SignUpScreen = () => {
+  const submitForm = () => {
+    //TODO: implementar submit form
+  };
+  return (
+    <Screen canGoback>
+      <Text bold preset="headingLarge" marginBottom="s32">
+        Criar uma conta
+      </Text>
+      <TextInput
+        label="Seu username"
+        placeholder="@"
+        errorMessage="Digite um username válido"
+        boxProps={{marginBottom: 's20'}}
+      />
+      <TextInput
+        label="Nome Completo"
+        placeholder="Digite seu nome completo"
+        errorMessage="Digite um nome válido"
+      />
+      <TextInput
+        label="E-mail"
+        placeholder="Digite seu E-mail"
+        errorMessage="Digite um username válido"
+        boxProps={{marginBottom: 's20'}}
+      />
+      <TextInput
+        label="Ssenha"
+        placeholder="Digite sua senha"
+        errorMessage="Digite um username válido"
+        RightComponent={<EyeOnIcon />}
+        boxProps={{marginBottom: 's40'}}
+      />
+      <Button onPress={submitForm} title="Criar minha conta" />
+    </Screen>
+  );
+};
