fb2d9902e953c8e238842a73eab8066cedd0087b
tonymatheus <tony.chaves14@gmail.com>
2023-09-05 09:08:16 -0300

    chore:Implement passwordInput component

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import {TextInput, TextInputProps} from '../TextInput/TextInput';
+import {Icon} from '../Icon/Icon';
+
+type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>;
+
+export const PasswordInput = (props: PasswordInputProps) => {
+  const [isSecureTextEntry, setIsSecureTextEntry] = useState(true);
+
+  const toggleSecureTextEntry = () => {
+    setIsSecureTextEntry(prev => !prev);
+  };
+
+  return (
+    <TextInput
+      secureTextEntry={isSecureTextEntry}
+      {...props}
+      RightComponent={
+        <Icon
+          onPress={toggleSecureTextEntry}
+          color="gray2"
+          name={isSecureTextEntry ? 'eyeOn' : 'eyeOff'}
+        />
+      }
+      boxProps={{marginBottom: 's40'}}
+    />
+  );
+};
