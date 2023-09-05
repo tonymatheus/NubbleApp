be9cff4a657d00a0314db1cbc9925b81271f6c37
tonymatheus <tony.chaves14@gmail.com>
2023-09-05 09:07:40 -0300

    chore:Add passwordInput Component and remove layout tests

--- src/screen/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screen/auth/SignUpScreen/SignUpScreen.tsx
@@ -4,6 +4,7 @@ import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {EyeOnIcon} from '../../../assets/icons/EyeOnIcon';
 import {Button} from '../../../components/Button/Button';
+import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 
 export const SignUpScreen = () => {
   const submitForm = () => {
@@ -20,52 +21,13 @@ export const SignUpScreen = () => {
         errorMessage="Digite um username válido"
         boxProps={{marginBottom: 's20'}}
       />
-      <TextInput
-        label="Nome Completo"
-        placeholder="Digite seu nome completo"
-        errorMessage="Digite um nome válido"
-      />
+      <TextInput label="Nome Completo" placeholder="Digite seu nome completo" />
       <TextInput
         label="E-mail"
         placeholder="Digite seu E-mail"
-        errorMessage="Digite um username válido"
         boxProps={{marginBottom: 's20'}}
       />
-      <TextInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        errorMessage="Digite um username válido"
-        RightComponent={<EyeOnIcon />}
-        boxProps={{marginBottom: 's40'}}
-      />
-      <TextInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        errorMessage="Digite um username válido"
-        RightComponent={<EyeOnIcon />}
-        boxProps={{marginBottom: 's40'}}
-      />
-      <TextInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        errorMessage="Digite um username válido"
-        RightComponent={<EyeOnIcon />}
-        boxProps={{marginBottom: 's40'}}
-      />
-      <TextInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        errorMessage="Digite um username válido"
-        RightComponent={<EyeOnIcon />}
-        boxProps={{marginBottom: 's40'}}
-      />
-      <TextInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        errorMessage="Digite um username válido"
-        RightComponent={<EyeOnIcon />}
-        boxProps={{marginBottom: 's40'}}
-      />
+      <PasswordInput label="Senha" placeholder="digite sua nova senha" />
       <Button onPress={submitForm} title="Criar minha conta" />
     </Screen>
   );
