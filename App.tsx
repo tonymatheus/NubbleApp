3ba73ccbac7c212361ae2ea3b5232f670f355f3c
tony.sousa <tony.sousa@ilia.digital>
2023-10-05 20:08:38 -0300

    chore:Add Router to app

--- App.tsx
+++ App.tsx
@@ -12,12 +12,13 @@ import {theme} from './theme/theme';
 import {LoginScreen} from './src/screen/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {SignUpScreen} from './src/screen/auth/SignUpScreen/SignUpScreen';
+import {Router} from './src/routes/Routes';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <SignUpScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
