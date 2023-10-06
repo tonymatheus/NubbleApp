513d092c7382a1e6adf14a4a3da61ea6b49dc41c
tonymatheus <tony.chaves14@gmail.com>
2023-10-05 23:01:16 -0300

    chore:Add typescript to our router

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,9 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SignUpScreen} from '../screen/auth/SignUpScreen/SignUpScreen';
 import {LoginScreen} from '../screen/auth/LoginScreen/LoginScreen';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  LoginScreen: undefined;
+  SignUpScreen: undefined;
+  // TODO: sucessScreen:icon,title,description
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const Router = () => {
   return (
@@ -13,6 +18,7 @@ export const Router = () => {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
+          fullScreenGestureEnabled: true,
         }}
         initialRouteName="LoginScreen">
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
