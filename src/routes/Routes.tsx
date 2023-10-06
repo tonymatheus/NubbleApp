dc9b6372c393239728f1e0f1d9a837ae03a353ea
tonymatheus <tony.chaves14@gmail.com>
2023-10-06 00:21:41 -0300

    chore:Pass props to successScreen

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,11 +3,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SignUpScreen} from '../screen/auth/SignUpScreen/SignUpScreen';
 import {LoginScreen} from '../screen/auth/LoginScreen/LoginScreen';
+import {SuccessScreen} from '../screen/auth/SuccessScreen/SuccessScreen';
+import {IconProps} from '../components/Icon/Icon';
 
 export type RootStackParamList = {
   LoginScreen: undefined;
   SignUpScreen: undefined;
-  // TODO: sucessScreen:icon,title,description
+  SuccessScreen: {
+    title: string;
+    description: string;
+    icon: Pick<IconProps, 'name' | 'color'>;
+  };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -23,6 +29,7 @@ export const Router = () => {
         initialRouteName="LoginScreen">
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
