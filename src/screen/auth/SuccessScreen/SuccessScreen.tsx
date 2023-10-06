08f25ee58df79c3689638acb5cce273fca05157e
tonymatheus <tony.chaves14@gmail.com>
2023-10-06 00:15:41 -0300

    chore:Add new Icons

---
+++ src/screen/auth/SuccessScreen/SuccessScreen.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {Icon} from '../../../components/Icon/Icon';
+import {Text} from '../../../components/Text/Text';
+import {Button} from '../../../components/Button/Button';
+import {Screen} from '../../../components/Screen/Screen';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../routes/Routes';
+
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SuccessScreen'>;
+
+export const SuccessScreen = ({route}: ScreenProps) => {
+  const goBackToBegin = () => {
+    //TODO: navegar para tela inicial
+  };
+
+  return (
+    <Screen>
+      <Icon {...route.params.icon} size={48} color={route.params.icon.color} />
+      <Text marginTop="s24" preset="headingLarge">
+        {route.params?.title}
+      </Text>
+      <Text marginTop="s16" preset="paragraphLarge">
+        {route.params.description}
+      </Text>
+      <Button
+        marginTop="s40"
+        title="Voltar ao início"
+        onPress={goBackToBegin}
+      />
+    </Screen>
+  );
+};
