5893b3f279bbe375ee8deb1faa4f6a4b15da3661
tonymatheus <tony.chaves14@gmail.com>
2023-11-11 07:44:27 -0300

    chore(routes.tsx): apply Authentication Flow Structure

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,12 +4,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {IconProps} from '@components';
-import {
-  SignUpScreen,
-  LoginScreen,
-  ForgotPasswordScreen,
-  SuccessScreen,
-} from '@screens';
+
+import {AppStack} from './AppStack';
+import {AuthStack} from './AuthStack';
 
 export type RootStackParamList = {
   LoginScreen: undefined;
@@ -22,25 +19,12 @@ export type RootStackParamList = {
   ForgotPasswordScreen: undefined;
 };
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
 export const Router = () => {
+  const authenticated = true;
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          fullScreenGestureEnabled: true,
-        }}
-        initialRouteName="LoginScreen">
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
-        <Stack.Screen
-          name="ForgotPasswordScreen"
-          component={ForgotPasswordScreen}
-        />
-      </Stack.Navigator>
+      {authenticated ? <AppStack /> : <AuthStack />}
     </NavigationContainer>
   );
 };
