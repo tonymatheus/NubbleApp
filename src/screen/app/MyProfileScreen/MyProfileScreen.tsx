26fe8de91d925526b4a0a6a13294e8f25e884657
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 02:32:45 -0300

    chore: add New AppScreens and pass to AppStack

---
+++ src/screen/app/MyProfileScreen/MyProfileScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Screen, Text} from '@components';
+
+export const MyProfileScreen = () => {
+  return (
+    <Screen>
+      <Text preset="headingLarge">New Post</Text>
+    </Screen>
+  );
+};
