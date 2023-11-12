c66d342fc7de46f774f1c1f1c67c1228fa10e48a
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 19:58:59 -0300

    chore: add AppTabScreenProps

--- src/screen/app/FavoriteScreen/FavoriteScreen.tsx
+++ src/screen/app/FavoriteScreen/FavoriteScreen.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 
 import {Screen, Text} from '@components';
+import {AppTabScreenProps} from '@routes';
 
-export const FavoriteScreen = () => {
+export const FavoriteScreen = (props: AppTabScreenProps<'FavoriteScreen'>) => {
   return (
     <Screen>
       <Text preset="headingLarge">Favorete</Text>
