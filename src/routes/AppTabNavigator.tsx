5cba6d14095e4c23a629afdb312d58bbf24aaa8e
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 20:40:55 -0300

    chore(apptabnavigatox.tsx): starts Bottom tab costumization

--- src/routes/AppTabNavigator.tsx
+++ src/routes/AppTabNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {Icon, Text} from '@components';
 import {
   FavoriteScreen,
   HomeScreen,
@@ -23,8 +24,30 @@ export const AppTabNavigator = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          paddingTop: 20,
+        },
       }}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen
+        options={{
+          tabBarLabel: ({focused}) => (
+            <Text
+              preset="paragraphCaption"
+              bold
+              color={focused ? 'primary' : 'backgroundContrast'}>
+              Inicio
+            </Text>
+          ),
+          tabBarIcon: ({focused}) => (
+            <Icon
+              name={focused ? 'homeFill' : 'home'}
+              color={focused ? 'primary' : 'backgroundContrast'}
+            />
+          ),
+        }}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
       <Tab.Screen name="NewPostScreen" component={NewPostScreen} />
       <Tab.Screen name="FavoriteScreen" component={FavoriteScreen} />
       <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} />
