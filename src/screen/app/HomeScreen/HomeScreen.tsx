084f3f19ee7e896a22c1cbc1299bd3f06906abdf
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 20:00:22 -0300

    chore: add tabScreenProps to Home Screen

--- src/screen/app/HomeScreen/HomeScreen.tsx
+++ src/screen/app/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
-import {AppScreenProps} from 'src/routes/navigationType';
-
 import {Button, Screen, Text} from '@components';
+import {AppTabScreenProps} from '@routes';
 
-export const HomeScreen = ({navigation}: AppScreenProps<'HomeScreen'>) => {
+export const HomeScreen = ({navigation}: AppTabScreenProps<'HomeScreen'>) => {
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
       <Button
         title="Settings"
         onPress={() => navigation.navigate('SettingsScreen')}
+      />
+      <Text preset="headingLarge">Home Screen</Text>
+      <Button
+        title="Favorites"
+        onPress={() => navigation.navigate('FavoriteScreen')}
       />
     </Screen>
   );
