d908c0829aa2ae747a4e15845a896d2aa5bf5de8
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 02:32:11 -0300

    chore: add New AppScreens to absolute path

--- src/screen/index.ts
+++ src/screen/index.ts
@@ -4,3 +4,7 @@ export * from './auth/SignUpScreen/SignUpScreen';
 export * from './auth/SuccessScreen/SuccessScreen';
 export * from './app/HomeScreen/HomeScreen';
 export * from './app/SettingsScreen/SettingsScreen';
+export * from './app/FavoriteScreen/FavoriteScreen';
+export * from './app/MyProfileScreen/MyProfileScreen';
+export * from './app/NewPostScreen/NewPostScreen';
+export * from './app/SettingsScreen/SettingsScreen';
