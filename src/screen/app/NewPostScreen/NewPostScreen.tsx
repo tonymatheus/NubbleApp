d4aff004d2603d0a713e10cc37497d4610d6a900
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 20:01:35 -0300

    chore: add TabSCreenProps to newPostScreen

--- src/screen/app/NewPostScreen/NewPostScreen.tsx
+++ src/screen/app/NewPostScreen/NewPostScreen.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
 
 import {Screen, Text} from '@components';
+import {AppTabScreenProps} from '@routes';
 
-export const NewPostScreen = () => {
+export const NewPostScreen = (props: AppTabScreenProps<'NewPostScreen'>) => {
   return (
     <Screen>
       <Text preset="headingLarge">New Post</Text>
