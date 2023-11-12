aca4869fbf0e0b0ff4bcf3e603645c698521f011
tonymatheus <tony.chaves14@gmail.com>
2023-11-12 20:02:52 -0300

    chore: add AppScreenProps to Settings Screen

--- src/screen/app/SettingsScreen/SettingsScreen.tsx
+++ src/screen/app/SettingsScreen/SettingsScreen.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 
 import {AppScreenProps} from 'src/routes/navigationType';
 
-import {Screen, Text} from '@components';
+import {Button, Screen, Text} from '@components';
 
-//type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
-
-export const SettingsScreen = ({}: AppScreenProps<'SettingsScreen'>) => {
+export const SettingsScreen = ({
+  navigation,
+}: AppScreenProps<'SettingsScreen'>) => {
   return (
     <Screen canGoback>
       <Text preset="headingLarge">Settings Screen</Text>
+      <Button
+        title="New Post"
+        onPress={() =>
+          navigation.navigate('AppTabNavigator', {
+            screen: 'NewPostScreen',
+          })
+        }
+      />
     </Screen>
   );
 };
