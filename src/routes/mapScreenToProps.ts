747ab747e1aacd74655226499423d294886ac878
tonymatheus <tony.chaves14@gmail.com>
2023-11-13 07:58:25 -0300

    chore: create utility type to export icons and names to tabBar

---
+++ src/routes/mapScreenToProps.ts
@@ -0,0 +1,42 @@
+import {IconProps} from '@components';
+import {AppTabBottomTabParamList} from '@routes';
+
+export const mapScreenToProps: Record<
+  keyof AppTabBottomTabParamList,
+  {
+    label: string;
+    icon: {
+      focused: IconProps['name'];
+      unfocused: IconProps['name'];
+    };
+  }
+> = {
+  HomeScreen: {
+    label: 'Início',
+    icon: {
+      focused: 'homeFill',
+      unfocused: 'home',
+    },
+  },
+  NewPostScreen: {
+    label: 'Novo Post',
+    icon: {
+      focused: 'newPost',
+      unfocused: 'newPost',
+    },
+  },
+  FavoriteScreen: {
+    label: 'Favoritos',
+    icon: {
+      focused: 'bookmark',
+      unfocused: 'bookmarkFill',
+    },
+  },
+  MyProfileScreen: {
+    label: 'Meu Perfil',
+    icon: {
+      focused: 'profile',
+      unfocused: 'profileFill',
+    },
+  },
+};
