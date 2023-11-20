0290827186ea20fd0f42593ad9cd3af3bbea63fa
tonymatheus <tony.chaves14@gmail.com>
2023-11-19 21:26:43 -0300

    chore: add postBottom To Post Item component

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -5,6 +5,7 @@ import {Post} from '@domain';
 import {Box} from '@components';
 
 import {PostActions} from './components/PostActions';
+import {PostBottom} from './components/PostBottom';
 import {PostHeader} from './components/PostHeader';
 import {PostImage} from './components/PostImage';
 
@@ -14,13 +15,18 @@ interface Props {
 
 export const PostItem = ({post}: Props) => {
   return (
-    <Box marginBottom="s24">
+    <Box marginBottom="s24" paddingHorizontal="s24">
       <PostHeader author={post.author} />
       <PostImage imageURL={post.imageURL} />
       <PostActions
         reactionCount={post.reactionCount}
         commentCount={post.commentCount}
         favoriteCount={post.favoriteCount}
+      />
+      <PostBottom
+        author={post.author}
+        commentCount={post.commentCount}
+        text={post.text}
       />
     </Box>
   );
