1caf866ea75dced09f27baf878a2de899729cf71
tonymatheus <tony.chaves14@gmail.com>
2023-11-24 10:14:57 -0300

    chore: add api directory

---
+++ src/api/apiTypes.ts
@@ -0,0 +1,20 @@
+export interface MetaDataPageAPI {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+  first_page: number;
+  first_page_url: string;
+  last_page_url: string;
+  next_page_url: string | null;
+  previous_page_url: string | null;
+}
+
+/**
+ * @description Interface que define o formato de uma página de dados da API
+ * @template Data tipo de dado da página
+ */
+export interface PageAPI<Data> {
+  meta: MetaDataPageAPI;
+  data: Data[];
+}
