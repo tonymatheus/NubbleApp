e1ddf52c02043a14cb1426577c35ab2117c7c2ca
tonymatheus <tony.chaves14@gmail.com>
2023-11-24 10:03:18 -0300

    refactor(postapi): add api calls in post Layer

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,13 +1,19 @@
-import {postListMock} from './postListMock';
-import {Post} from './types';
+import {PageAPI} from '@api';
 
-const getList = async (): Promise<Post[]> => {
-  await new Promise(res =>
-    setTimeout(() => {
-      res();
-    }, 1000),
-  );
-  return postListMock;
+import {PostAPI} from './postTypes';
+
+const getList = async (): Promise<PageAPI<PostAPI>> => {
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer NA.F6zvhk1eVTIgXIp1K0IjuiFqaqnuruDdcMKUz_kxb3SpB19Yp3E1eCmc9l2b',
+    },
+  });
+
+  let data: PageAPI<PostAPI> = await response.json();
+
+  return data;
 };
 
 export const postApi = {
