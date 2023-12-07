7d56f8a4ac146d040c87756ea072bc13553f96c2
tonymatheus <tony.chaves14@gmail.com>
2023-12-07 07:51:11 -0300

    chore: add promisse to sumulate request delay

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -3,6 +3,7 @@ import {PageAPI, api} from '@api';
 import {PostAPI} from './postTypes';
 
 const getList = async (): Promise<PageAPI<PostAPI>> => {
+  await new Promise(resolve => setTimeout(() => resolve(''), 2000));
   const response = await api.get<PageAPI<PostAPI>>('user/post');
 
   return response.data;
