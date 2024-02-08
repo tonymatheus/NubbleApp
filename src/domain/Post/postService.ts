ecbd8dcebef327020452aa59d4634049d156c2ed
tonymatheus <tony.chaves14@gmail.com>
2024-02-08 12:02:43 -0300

    refactor(postservice): add meta and data to getlist

--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,11 +1,17 @@
+import {apiAdapter} from '@api';
+import {Page} from '@types';
+
 import {postAdapter} from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
 
-const getList = async (page: number): Promise<Post[]> => {
+const getList = async (page: number): Promise<Page<Post>> => {
   const postPageAPI = await postApi.getList({page, per_page: 10});
 
-  return postPageAPI.data.map(postAdapter.toPost);
+  return {
+    data: postPageAPI.data.map(postAdapter.toPost),
+    meta: apiAdapter.toMetaDataPage(postPageAPI.meta),
+  };
 };
 
 export const postService = {
