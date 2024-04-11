3f3ec1d99de81578aa029589e99a06c084999bd3
tonymatheus <tony.chaves14@gmail.com>
2024-04-11 05:53:44 -0300

    chore: add postScreenProps and add page to Stack

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import {NavigatorScreenParams} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {SettingsScreen} from '@screens';
+import {SettingsScreen, PostCommentScreen} from '@screens';
 
 import {AppTabBottomTabParamList, AppTabNavigator} from './AppTabNavigator';
 
 export type AppStackParamList = {
   AppTabNavigator: NavigatorScreenParams<AppTabBottomTabParamList>;
   SettingsScreen: undefined;
+  PostCommentScreen: {
+    postId: string;
+  };
 };
 
 const Stack = createNativeStackNavigator<AppStackParamList>();
@@ -24,6 +27,7 @@ export const AppStack = () => {
       initialRouteName="AppTabNavigator">
       <Stack.Screen component={AppTabNavigator} name="AppTabNavigator" />
       <Stack.Screen component={SettingsScreen} name="SettingsScreen" />
+      <Stack.Screen component={PostCommentScreen} name="PostCommentScreen" />
     </Stack.Navigator>
   );
 };
