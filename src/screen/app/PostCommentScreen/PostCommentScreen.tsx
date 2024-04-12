3626c0fda0bd27ad349fd967904cb8eafd12c551
tonymatheus <tony.chaves14@gmail.com>
2024-04-12 06:43:40 -0300

    chore: update usecommentList

--- src/screen/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screen/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import {usePostCommentList} from '@domain';
+
 import {Box, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
 export const PostCommentScreen = ({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   route,
 }: AppScreenProps<'PostCommentScreen'>) => {
-  //route.params.postId
+  const postID = route?.params?.postId;
+
+  usePostCommentList(postID);
+
   return (
     <Screen title="Comentários" canGoBack>
       <Box>
