eb5e931646e5baf4b192f0ee3e46fcb6415d6480
tonymatheus <tony.chaves14@gmail.com>
2024-05-05 07:58:18 -0300

    chore: add export to useCommentRemove

--- src/domain/PostComment/index.ts
+++ src/domain/PostComment/index.ts
@@ -2,3 +2,4 @@ export * from './postCommentTypes';
 export * from './useCases/usePostCommentList';
 export * from './postCommentService';
 export * from './useCases/usePostCommentCreate';
+export * from './useCases/usePostCommentRemove';
