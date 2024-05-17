4ebfb9e8deb73436c423168c231b4b92c937ec2b
tonymatheus <tony.chaves14@gmail.com>
2024-05-17 18:13:23 -0300

    refactor: remove and abstract code to another folder structure

--- src/services/toast/useToast.tsx
+++ src/services/toast/useToast.tsx
@@ -1,50 +1,6 @@
-import React, {createContext, useContext, useState} from 'react';
-
-interface Toast {
-  message: string;
-  type?: 'success' | 'error';
-  duration?: number;
-  actions?: {
-    title: string;
-    onPress: () => void;
-  };
-}
-
-interface ToastService {
-  toast: Toast | null;
-  showToast: (toast: Toast) => void;
-  hideToast: () => void;
-}
-
-const ToastContext = createContext<ToastService>({
-  toast: null,
-  showToast: () => {},
-  hideToast: () => {},
-});
-
-export const ToastProvider = ({children}: React.PropsWithChildren) => {
-  const [toast, setToast] = useState<ToastService['toast']>(null);
-
-  const showToast = (_toast: Toast) => {
-    setToast(_toast);
-  };
-
-  const hideToast = () => {
-    setToast(null);
-  };
-
-  return (
-    <ToastContext.Provider value={{toast, showToast, hideToast}}>
-      {children}
-    </ToastContext.Provider>
-  );
-};
+import {ToastService} from './toastTypes';
+import {useToastContext} from './useToastContext';
 
 export const useToast = (): ToastService => {
-  const {toast, showToast, hideToast} = useContext(ToastContext);
-  return {
-    toast,
-    showToast,
-    hideToast,
-  };
+  return useToastContext();
 };
