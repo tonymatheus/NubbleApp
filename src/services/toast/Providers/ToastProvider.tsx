57346e68e294d318e8165f76f1aeb3e2e3d3c59e
tonymatheus <tony.chaves14@gmail.com>
2024-05-17 18:14:11 -0300

    feat: implement new toastProvider

---
+++ src/services/toast/Providers/ToastProvider.tsx
@@ -0,0 +1,27 @@
+import React, {createContext, useState} from 'react';
+
+import {Toast, ToastService} from '../toastTypes';
+
+export const ToastContext = createContext<ToastService>({
+  toast: null,
+  showToast: () => {},
+  hideToast: () => {},
+});
+
+export const ToastProvider = ({children}: React.PropsWithChildren) => {
+  const [toast, setToast] = useState<ToastService['toast']>(null);
+
+  const showToast = (_toast: Toast) => {
+    setToast(_toast);
+  };
+
+  const hideToast = () => {
+    setToast(null);
+  };
+
+  return (
+    <ToastContext.Provider value={{toast, showToast, hideToast}}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
