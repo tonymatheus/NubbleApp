6765373387fc584eb5fcb41a4775ae8aff1fd4f0
tonymatheus <tony.chaveas14@gmail.com>
2024-11-14 09:19:34 -0300

    chore: add Error Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -17,6 +17,7 @@ import {CheckIcon} from '../../assets/icons/CheckIcon';
 import {checkRoundIcon} from '../../assets/icons/checkRoundIcon';
 import {ChevronRightIcon} from '../../assets/icons/ChevronRightIcon';
 import {CommentIcon} from '../../assets/icons/CommentIcon';
+import {ErrorRoundIcon} from '../../assets/icons/ErrorRoundIcon';
 import {EyeOffIcon} from '../../assets/icons/EyeOffIcon';
 import {EyeOnIcon} from '../../assets/icons/EyeOnIcon';
 import {FlashOffIcon} from '../../assets/icons/FlashOffIcon';
@@ -78,6 +79,7 @@ const iconRegistry = {
   chatOn: ChatOnIcon,
   check: CheckIcon,
   checkRound: checkRoundIcon,
+  errorRound: ErrorRoundIcon,
   comment: CommentIcon,
   chevronRight: ChevronRightIcon,
   eyeOn: EyeOnIcon,
