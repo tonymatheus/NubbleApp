0688635d1d318f734fc629db521214c373924fc1
tonymatheus <tony.chaveas14@gmail.com>
2024-11-14 07:09:27 -0300

    refactor: update context with zustend implementation

--- src/screen/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screen/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Alert, Pressable} from 'react-native';
 
 import {PostComment, postCommentService, usePostCommentRemove} from '@domain';
-import {useToast} from '@services';
+import {useToastService} from '@services';
 
 import {Box, ProfileAvatar, Text} from '@components';
 
@@ -19,7 +19,7 @@ export const PostCommentItem = ({
   userId,
   postAuthorId,
 }: Props) => {
-  const {showToast} = useToast();
+  const {showToast} = useToastService();
   const {mutate} = usePostCommentRemove({
     onSuccess: () => {
       onRemoveComment();
