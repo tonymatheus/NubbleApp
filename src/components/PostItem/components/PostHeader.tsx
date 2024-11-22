5a2c47e8c62161fcb76be1b03c01fedc0657acf8
tonymatheus <tony.chaves14@gmail.com>
2024-11-22 11:31:47 -0300

    refactor: add a touch button to navigate to profileScreen just to test

--- src/components/PostItem/components/PostHeader.tsx
+++ src/components/PostItem/components/PostHeader.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import {TouchableWithoutFeedback} from 'react-native';
 
 import {Post} from '@domain';
+import {useNavigation} from '@react-navigation/native';
 
 import {Box, Text, ProfileAvatar} from '@components';
 
 type Props = Pick<Post, 'author'>;
 
 export const PostHeader = ({author}: Props) => {
+  const navigation = useNavigation();
   return (
-    <Box marginBottom="s24">
-      <Box flexDirection="row" alignItems="center" marginBottom="s16">
-        <ProfileAvatar imageURL={author.profileURL} />
-        <Text preset="paragraphMedium" marginLeft="s12">
-          {author.userName}
-        </Text>
+    <TouchableWithoutFeedback
+      onPress={() => navigation.navigate('ProfileScreen', {userId: 1})}>
+      <Box marginBottom="s24">
+        <Box flexDirection="row" alignItems="center" marginBottom="s16">
+          <ProfileAvatar imageURL={author.profileURL} />
+          <Text preset="paragraphMedium" marginLeft="s12">
+            {author.userName}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </TouchableWithoutFeedback>
   );
 };
