8e77cadad061d8ade856d8e434914f1f754ae739
tonymatheus <tony.chaves14@gmail.com>
2025-03-04 02:13:37 -0300

    refactor(homescreen): update values and ident

--- src/screen/app/HomeScreen/HomeScreen.tsx
+++ src/screen/app/HomeScreen/HomeScreen.tsx
@@ -16,43 +16,49 @@ import {AppTabScreenProps} from '@routes';
 import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
-export const HomeScreen = ({}: AppTabScreenProps<'HomeScreen'>) => {
-  const {list, isPostListEmpty, refresh, isLoading, isError, fetchNextPage} =
-    usePostList();
+export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+  const {
+    list: postList,
+    isError,
+    isLoading,
+    refresh,
+    fetchNextPage,
+  } = usePostList();
+
   const flatListRef = React.useRef<FlatList<Post>>(null);
   useScrollToTop(flatListRef);
 
-  const renderPostItem = ({item}: ListRenderItemInfo<Post>) => {
+  function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
-  };
+  }
 
   return (
     <Screen style={$screen}>
       <FlatList
         ref={flatListRef}
         showsVerticalScrollIndicator={false}
-        data={list}
+        data={postList}
         keyExtractor={item => item.id.toString()}
+        renderItem={renderItem}
         onEndReached={fetchNextPage}
         onEndReachedThreshold={0.1}
         refreshing={isLoading}
         refreshControl={
           <RefreshControl refreshing={isLoading} onRefresh={refresh} />
         }
-        renderItem={renderPostItem}
-        contentContainerStyle={{flex: isPostListEmpty}}
+        contentContainerStyle={{flex: postList.length === 0 ? 1 : undefined}}
         ListHeaderComponent={<HomeHeader />}
         ListEmptyComponent={
-          <HomeEmpty error={isError} loading={isLoading} refetch={refresh} />
+          <HomeEmpty refetch={refresh} error={isError} loading={isLoading} />
         }
       />
     </Screen>
   );
-};
+}
 
 const $screen: StyleProp<ViewStyle> = {
+  paddingTop: 0,
   paddingBottom: 0,
   paddingHorizontal: 0,
-  paddingTop: 0,
   flex: 1,
 };
