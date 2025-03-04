48f613caf19db8acfb99b590a76c6ea97c613ec7
tonymatheus <tony.chaves14@gmail.com>
2025-03-04 02:16:30 -0300

    chore: update token

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MTc.aesGQMcwCrarcKPPQnAVwOdVRsUS68FutZWr5LXCVtfxwN4T5Sk0-gvLy4ET',
+      'Bearer MTg.RvIYZVzU3BCpQjN1cXQvmgK7z_VsnRx3GIt6naB2mdha0j489pBTm81pINVH',
   },
 });
